edfd321316212ddd545b4f2c4e27c23754b3aeb2
たのけん <tanoken729@gmail.com>
2021-03-12 00:10:15 +0900

    年月日の値を生成した外部jsファイルを、vueコンポーネントに読み込む

--- src/components/BasicForm.vue
+++ src/components/BasicForm.vue
@@ -7,71 +7,17 @@
     </div>
     <div class="body">
       <p class="genre">-性別-</p>
-        <label><input name="q1" type="radio">男性</label>
-        <label><input name="q1" type="radio">女性</label>
+        <label><input type="radio" name="gender" value="男性">男性</label>
+        <label><input type="radio" name="gender" value="女性">女性</label>
       <p class="genre">-生年月日-</p>
       <select name="year" id="id_year">
-        <option value="">-</option>
-        <option value="1990">1990（平成2）</option>
-        <option value="1991">1991（平成3）</option>
-        <option value="1992">1992（平成4）</option>
-        <option value="1993">1993（平成5）</option>
-        <option value="1994">1994（平成6）</option>
-        <option value="1995">1995（平成7）</option>
-        <option value="1996">1996（平成8）</option>
-        <option value="1997">1997（平成9）</option>
-        <option value="1998">1998（平成10）</option>
-        <option value="1999">1999（平成11）</option>
-        <option value="2000">2000（平成12）</option>
+        <option v-for="(year, key) in years" :key="key">{{ year }}</option>
       </select>年
       <select name="month" id="id_month">
-        <option value="">-</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
-        <option value="11">11</option>
-        <option value="12">12</option>
+        <option v-for="(month, key) in months" :key="key">{{ month }}</option>
       </select>月
       <select name="day" id="id_day">
-        <option value="">-</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
-        <option value="11">11</option>
-        <option value="12">12</option>
-        <option value="13">13</option>
-        <option value="14">14</option>
-        <option value="15">15</option>
-        <option value="16">16</option>
-        <option value="17">17</option>
-        <option value="18">18</option>
-        <option value="19">19</option>
-        <option value="20">20</option>
-        <option value="21">21</option>
-        <option value="22">22</option>
-        <option value="23">23</option>
-        <option value="24">24</option>
-        <option value="25">25</option>
-        <option value="26">26</option>
-        <option value="27">27</option>
-        <option value="28">28</option>
-        <option value="29">29</option>
-        <option value="30">30</option>
-        <option value="31">31</option>
+        <option v-for="(day, key) in days" :key="key">{{ day }}</option>
       </select>日
     </div>
   </div>
@@ -82,18 +28,38 @@
 </template>
 
 <script>
-// import common from '@/static/js/option';
+import { yearList, monthList, dayList } from '@/utiles/definition';
 
-// export default {
-//   data() {
+export default {
+  data() {
+    return {
+      years: yearList,
+      months: monthList,
+      days: dayList,
+    }
+  },
+  // methods() {
+  //   optionLoop(1950, 2000, yearList);
+  //   yearList: this.yearList
+  // }
+  // method: {
+  //   optionLoop(){
+  //     const {start, end} = optionLoop({
+  //     })
+  //   }
+  // }
+  // data() {
 
-//   },
-//   methods: {
-//     optionLoop(start, end, id, this_day) {
-//       common.optionLoop(start, end, id, this_day);
-//     }
-//   }
-// }
+  // },
+  // methods: {
+  //   // optionLoop(start, end) {
+  //   //   definition.optionLoop(start, end);
+  //   // }
+  //   addTodo(e) {
+  //     this.$store,commit('todo/add', e.target.value);
+  //   }
+  // }
+}
 </script>
   <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
@@ -134,6 +100,10 @@
   padding-bottom: 10px;
   line-height: 30px;
   text-align: left;
+}
+
+.body input {
+  vertical-align:middle;
 }
 
 .genre {
